aee65ecf0df9379ed61b01606d8d9fb34c1a53fe
Pierre Duchateau <pad@dabla.com>
2020-10-16 19:48:58 +0200

    handleProjectNotificationDateSet triggers GFDueDateSet and DCRDueDateSet

---
+++ src/modules/eventStore/StoredEvent.ts
@@ -0,0 +1,42 @@
+import {
+  CandidateNotificationForPeriodeFailed,
+  CandidateNotifiedForPeriode,
+  LegacyProjectEventSourced,
+  LegacyProjectSourced,
+  PeriodeNotified,
+  ProjectCertificateGenerated,
+  ProjectCertificateGenerationFailed,
+  ProjectDCRDueDateSet,
+  ProjectDCRRemoved,
+  ProjectDCRSubmitted,
+  ProjectGFDueDateSet,
+  ProjectGFReminded,
+  ProjectGFRemoved,
+  ProjectGFSubmitted,
+  ProjectImported,
+  ProjectNotified,
+  ProjectDataCorrected,
+  ProjectNotificationDateSet,
+  ProjectReimported,
+} from '../project/events'
+
+export type StoredEvent =
+  | ProjectNotified
+  | ProjectCertificateGenerated
+  | ProjectCertificateGenerationFailed
+  | PeriodeNotified
+  | CandidateNotificationForPeriodeFailed
+  | CandidateNotifiedForPeriode
+  | ProjectDCRDueDateSet
+  | ProjectGFDueDateSet
+  | LegacyProjectEventSourced
+  | LegacyProjectSourced
+  | ProjectImported
+  | ProjectReimported
+  | ProjectDCRRemoved
+  | ProjectDCRSubmitted
+  | ProjectGFSubmitted
+  | ProjectGFRemoved
+  | ProjectGFReminded
+  | ProjectDataCorrected
+  | ProjectNotificationDateSet
